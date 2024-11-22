2adb206bea4f92408b77f2c9f2ee7f0a6320b5e1
Ibrahim E.Gad <gad.i@turing.com>
2024-11-22 16:34:55 +0200

    Improve the code for fetching from the sheet

--- src/stores/usePromptsBank.tsx
+++ src/stores/usePromptsBank.tsx
@@ -63,127 +63,41 @@ export const usePromptsBank = create<PromptsBankState>()(
 
                         for (let index = 0; index < lines.length; index++) {
                             const line = lines[index];
-                            switch (index + 1) {
-                                case 3:
-                                    line.filter(cell => cell.trim() !== '').forEach((cell, i) => {
-                                        const name = lines[index-1][i] || `Example ${i + 1}`;
-                                            prompts.push({
-                                            id: uuidv4(),
-                                            format: SystemPromptFormat.MARKDOWN,
-                                            name: name,
-                                            prompt: cell,
-                                            addressee: "assistant"
-                                        });
+                            const formats:  { [key: number]: SystemPromptFormat } = {
+                                3: SystemPromptFormat.MARKDOWN,
+                                6: SystemPromptFormat.TEXT,
+                                9: SystemPromptFormat.MARKDOWN_WITH_XML,
+                                12: SystemPromptFormat.TEXT_WITH_XML,
+                                15: SystemPromptFormat.FULL_XML,
+                                18: SystemPromptFormat.MARKDOWN,
+                                21: SystemPromptFormat.TEXT,
+                                24: SystemPromptFormat.MARKDOWN_WITH_XML,
+                                27: SystemPromptFormat.TEXT_WITH_XML,
+                                30: SystemPromptFormat.FULL_XML
+                            };
+                            const addressees: { [key: number]: string } = {
+                                3: "assistant",
+                                6: "assistant",
+                                9: "assistant",
+                                12: "assistant",
+                                15: "assistant",
+                                18: "third-person",
+                                21: "third-person",
+                                24: "third-person",
+                                27: "third-person",
+                                30: "third-person"
+                            }
+                            if ([3, 6, 9, 12, 15, 18, 21, 24, 27, 30].includes(index+1)) {
+                                line.filter(cell => cell.trim() !== '').forEach((cell, i) => {
+                                    const name = lines[index-1][i] || `Example ${i + 1}`;
+                                    prompts.push({
+                                        id: uuidv4(),
+                                        format: formats[index+1],
+                                        name: name,
+                                        prompt: cell,
+                                        addressee: addressees[index+1]
                                     });
-                                    break;
-                                case 6:
-                                    line.filter(cell => cell.trim() !== '').forEach((cell, i) => {
-                                        const name = lines[index-1][i] || `Example ${i + 1}`;
-                                        prompts.push({
-                                            id: uuidv4(),
-                                            format: SystemPromptFormat.TEXT,
-                                            name: name,
-                                            prompt: cell,
-                                            addressee: "assistant"
-                                        });
-                                    });
-                                    break;
-                                case 9:
-                                    line.filter(cell => cell.trim() !== '').forEach((cell, i) => {
-                                        const name = lines[index-1][i] || `Example ${i + 1}`;
-                                        prompts.push({
-                                            id: uuidv4(),
-                                            format: SystemPromptFormat.MARKDOWN_WITH_XML,
-                                            name: name,
-                                            prompt: cell,
-                                            addressee: "assistant"
-                                        });
-                                    });
-                                    break;
-                                case 12:
-                                    line.filter(cell => cell.trim() !== '').forEach((cell, i) => {
-                                        const name = lines[index-1][i] || `Example ${i + 1}`;
-                                        prompts.push({
-                                            id: uuidv4(),
-                                            format: SystemPromptFormat.TEXT_WITH_XML,
-                                            name: name,
-                                            prompt: cell,
-                                            addressee: "assistant"
-                                        });
-                                    });
-                                    break;
-                                case 15:
-                                    line.filter(cell => cell.trim() !== '').forEach((cell, i) => {
-                                        const name = lines[index-1][i] || `Example ${i + 1}`;
-                                        prompts.push({
-                                            id: uuidv4(),
-                                            format: SystemPromptFormat.FULL_XML,
-                                            name: name,
-                                            prompt: cell,
-                                            addressee: "assistant"
-                                        });
-                                    });
-                                    break;
-                                case 18:
-                                    line.filter(cell => cell.trim() !== '').forEach((cell, i) => {
-                                        const name = lines[index-1][i] || `Example ${i + 1}`;
-                                        prompts.push({
-                                            id: uuidv4(),
-                                            format: SystemPromptFormat.MARKDOWN,
-                                            name: name,
-                                            prompt: cell,
-                                            addressee: "third-person"
-                                        });
-                                    });
-                                    break;
-                                case 21:
-                                    line.filter(cell => cell.trim() !== '').forEach((cell, i) => {
-                                        const name = lines[index-1][i] || `Example ${i + 1}`;
-                                        prompts.push({
-                                            id: uuidv4(),
-                                            format: SystemPromptFormat.TEXT,
-                                            name: name,
-                                            prompt: cell,
-                                            addressee: "third-person"
-                                        });
-                                    });
-                                    break;
-                                case 24:
-                                    line.filter(cell => cell.trim() !== '').forEach((cell, i) => {
-                                        const name = lines[index-1][i] || `Example ${i + 1}`;
-                                        prompts.push({
-                                            id: uuidv4(),
-                                            format: SystemPromptFormat.MARKDOWN_WITH_XML,
-                                            name: name,
-                                            prompt: cell,
-                                            addressee: "third-person"
-                                        });
-                                    });
-                                    break;
-                                case 27:
-                                    line.filter(cell => cell.trim() !== '').forEach((cell, i) => {
-                                        const name = lines[index-1][i] || `Example ${i + 1}`;
-                                        prompts.push({
-                                            id: uuidv4(),
-                                            format: SystemPromptFormat.TEXT_WITH_XML,
-                                            name: name,
-                                            prompt: cell,
-                                            addressee: "third-person"
-                                        });
-                                    });
-                                    break;
-                                case 30:
-                                    line.filter(cell => cell.trim() !== '').forEach((cell, i) => {
-                                        const name = lines[index-1][i] || `Example ${i + 1}`;
-                                        prompts.push({
-                                            id: uuidv4(),
-                                            format: SystemPromptFormat.FULL_XML,
-                                            name: name,
-                                            prompt: cell,
-                                            addressee: "third-person"
-                                        });
-                                    });
-                                    break;
+                                });
                             }
                         }
                         return prompts;
